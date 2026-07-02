73943191d869131d084a84668efbdd5dae1e0d11
Sebastian Pulido <sebastianpulido12p@gmail.com>
2026-07-02 15:08:03 -0500

    Palabra reservada this

--- clases/persona.php
+++ clases/persona.php
@@ -2,8 +2,15 @@
 
 class persona 
 {
+    public $nombre;
+    public $edad;
+    public $correo;
+    public $apellido;
+
     public function saludar()
     {
-        echo "Mi nombre es:";
-    }
+        return "Mi nombre es: " . $this->nombre . " " . $this->apellido . " tengo " . $this->edad . " años. Mi correo es: " . $this->correo . "<br>";
+        return "Mi nombre es: " . $this->nombre . " " . $this->apellido . "tengo" . $this->edad . "años. Mi correo es: " . $this->correo . "<br>";
+    }    
+
 }
--- public/index.php
+++ public/index.php
@@ -2,7 +2,23 @@
 
 require_once("../clases/persona.php");
 
-$persona = new persona();
+$persona1 = new persona();
+$persona2 = new persona();
 
-echo $persona->saludar();
+
+$persona1->nombre = "Sebastian";
+$persona1->edad = 18;
+$persona1->correo = "sebastian@gmail.com";
+$persona1->apellido = "pulido";
+
+$persona2->nombre = "kakaroto";
+$persona2->edad = 20;
+$persona2->correo = "kakaroto@gmail.com";
+$persona2->apellido = "usumaki";
+
+
+
+echo $persona1->saludar();
+echo $persona2->saludar();
+
 ?>
