08c90913c0120cc04998530fc0a6c6f757628d74
Sebastian Pulido <sebastianpulido12p@gmail.com>
2026-07-02 15:28:49 -0500

    METODO CONSTRUCTOR

--- clases/persona.php
+++ clases/persona.php
@@ -7,6 +7,15 @@ class persona
     public $correo;
     public $apellido;
 
+    
+    public function __construct($nombre, $edad, $correo, $apellido)
+    { 
+       $this->nombre= $nombre;
+       $this->edad=$edad;
+       $this->correo=$correo;
+       $this->apellido=$apellido;
+    }
+
     public function saludar()
     {
         return "Mi nombre es: " . $this->nombre . " " . $this->apellido . " tengo " . $this->edad . " años. Mi correo es: " . $this->correo . "<br>";
--- public/index.php
+++ public/index.php
@@ -2,19 +2,8 @@
 
 require_once("../clases/persona.php");
 
-$persona1 = new persona();
-$persona2 = new persona();
-
-
-$persona1->nombre = "Sebastian";
-$persona1->edad = 18;
-$persona1->correo = "sebastian@gmail.com";
-$persona1->apellido = "pulido";
-
-$persona2->nombre = "kakaroto";
-$persona2->edad = 20;
-$persona2->correo = "kakaroto@gmail.com";
-$persona2->apellido = "usumaki";
+$persona1 = new persona("Sebastian", 18, "sebastian@gmail.com", "pulido");
+$persona2 = new persona("kakaroto", 20, "kakaroto@gmail.com", "usumaki");
 
 
 
